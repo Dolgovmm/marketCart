a0356b501e7a1b4ae9784e0c5ae4fd7027065dc4
Dolgov <dolgowmm@gmail.com>
2018-01-22 19:06:08 +0700

    add connection pool

--- src/main/java/ru/dolgov/market/dao/CartDAOImpl.java
+++ src/main/java/ru/dolgov/market/dao/CartDAOImpl.java
@@ -1,5 +1,6 @@
 package ru.dolgov.market.dao;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,38 +15,76 @@ public class CartDAOImpl implements CartDAO{
 	private final String UPDATE_CART = "update cart set client_id = ?, total_items = ?, products_cost = ? where id = ?";
 	private final String GET_ID_CART = "select @@IDENTITY";
 	
-	private PreparedStatement preparedStatementInsertCart;
-	private PreparedStatement preparedStatementUpdateCart;
-	
 	public void saveCart(Cart cart) throws SQLException {
 		
-		preparedStatementInsertCart.setInt(1, cart.getClient().getId());
-		preparedStatementInsertCart.setInt(2, cart.getTotalItems());
-		preparedStatementInsertCart.setInt(3, cart.getProductsCost());
-		preparedStatementInsertCart.executeUpdate();
-		
-		Statement statement = DbConnection.getConnection().createStatement();
-		ResultSet rs = statement.executeQuery(GET_ID_CART);
-		int id = -1;
-		if (rs.next()) {
-			id = rs.getInt(1);
-		}
-		cart.setId(id);
+		Connection connection = null;
+		try {
+			connection = DbConnection.getConnection();
+			
+			PreparedStatement preparedStatement = connection.prepareStatement(INSERT_CART);
+			
+			preparedStatement.setInt(1, cart.getClient().getId());
+			preparedStatement.setInt(2, cart.getTotalItems());
+			preparedStatement.setInt(3, cart.getProductsCost());
+			preparedStatement.executeUpdate();
+			
+			Statement statement = DbConnection.getConnection().createStatement();
+			ResultSet rs = statement.executeQuery(GET_ID_CART);
+			int id = -1;
+			if (rs.next()) {
+				id = rs.getInt(1);
+			}
+			cart.setId(id);
+			
+			rs.close();
+			statement.close();
+			preparedStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new SQLException(e);
+		}finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}catch (Exception ignore) {}
+			}
+		}		
 	}
 	
 	public void updateCart(Cart cart) throws SQLException {
 		
-		preparedStatementUpdateCart.setInt(1, cart.getClient().getId());
-		preparedStatementUpdateCart.setInt(2, cart.getTotalItems());
-		preparedStatementUpdateCart.setInt(3, cart.getProductsCost());
-		preparedStatementUpdateCart.setInt(4, cart.getId());
-		preparedStatementUpdateCart.executeUpdate();
-		
+		Connection connection = null;
+		try {
+			
+			connection = DbConnection.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_CART);
+			
+			preparedStatement.setInt(1, cart.getClient().getId());
+			preparedStatement.setInt(2, cart.getTotalItems());
+			preparedStatement.setInt(3, cart.getProductsCost());
+			preparedStatement.setInt(4, cart.getId());
+			preparedStatement.executeUpdate();
+			
+			Statement statement = DbConnection.getConnection().createStatement();
+			ResultSet rs = statement.executeQuery(GET_ID_CART);
+			int id = -1;
+			if (rs.next()) {
+				id = rs.getInt(1);
+			}
+			cart.setId(id);
+			
+			rs.close();
+			statement.close();
+			preparedStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new SQLException(e);
+		}finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}catch (Exception ignore) {}
+			}
+		}			
 	}
-	
-	public CartDAOImpl() throws SQLException {
-		preparedStatementInsertCart = DbConnection.getConnection().prepareStatement(INSERT_CART);
-		preparedStatementUpdateCart = DbConnection.getConnection().prepareStatement(UPDATE_CART);
-	}
-	
 }
--- src/main/java/ru/dolgov/market/dao/ClientDAOImpl.java
+++ src/main/java/ru/dolgov/market/dao/ClientDAOImpl.java
@@ -1,5 +1,6 @@
 package ru.dolgov.market.dao;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,50 +11,78 @@ import ru.dolgov.market.domain.Client;
 import ru.dolgov.market.jdbc.DbConnection;
 
 public class ClientDAOImpl implements ClientDAO{
-	private final String GET_CLIENT_BY_ID = "select * from Clients where email = ?;";
+	private final String GET_CLIENT_BY_EMAIL = "select * from Clients where email = ?;";
 	private final String SAVE_CLIENT = "insert into Clients (name, email, phonenumber) values (?, ?, ?)";
 	private final String GET_ID_CLIENT = "select @@IDENTITY";
 	
-	private PreparedStatement preparedStatementGetById;
-	private PreparedStatement preparedStatementSaveClient;
-
 	public Client getClientByEmail(String email) throws SQLException {
-		
-		preparedStatementGetById.setString(1, email);
-		ResultSet rs = preparedStatementGetById.executeQuery();
-		
-		Client client = new Client();
-		
-		if (rs.next()) {
-			client.setId(rs.getInt("id"));
-			client.setName(rs.getString("name"));
-			client.setPhoneNumber(rs.getString("phonenumber"));
-			client.setEmail(rs.getString("email"));
+		Connection connection = null;
+		try {
+			connection = DbConnection.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(GET_CLIENT_BY_EMAIL);
+			
+			preparedStatement.setString(1, email);
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			Client client = new Client();
+			
+			if (rs.next()) {
+				client.setId(rs.getInt("id"));
+				client.setName(rs.getString("name"));
+				client.setPhoneNumber(rs.getString("phonenumber"));
+				client.setEmail(rs.getString("email"));
+			}
+			
+			rs.close();
+			preparedStatement.close();
+			
+			return client;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new SQLException(e);
+		}finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}catch (Exception ignore) {}
+			}
 		}
-		
-		return client;
-
 	}
 	
 	public void saveClient(Client client) throws SQLException {
-		
-		preparedStatementSaveClient.setString(1, client.getName());
-		preparedStatementSaveClient.setString(2, client.getEmail());
-		preparedStatementSaveClient.setString(3, client.getPhoneNumber());
-		preparedStatementSaveClient.execute();
-		
-		Statement statement = DbConnection.getConnection().createStatement();
-		ResultSet rs = statement.executeQuery(GET_ID_CLIENT);
-		int id = -1;
-		if (rs.next()) {
-			id = rs.getInt(1);
-		}
-		client.setId(id);		
+		Connection connection = null;
+		try {
+			connection = DbConnection.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(SAVE_CLIENT);
+			
+			preparedStatement.setString(1, client.getName());
+			preparedStatement.setString(2, client.getEmail());
+			preparedStatement.setString(3, client.getPhoneNumber());
+			preparedStatement.execute();
+			
+			Statement statement = DbConnection.getConnection().createStatement();
+			ResultSet rs = statement.executeQuery(GET_ID_CLIENT);
+			int id = -1;
+			if (rs.next()) {
+				id = rs.getInt(1);
+			}
+			client.setId(id);	
+			
+			rs.close();
+			statement.close();
+			preparedStatement.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new SQLException(e);
+		}finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}catch (Exception ignore) {}
+			}
+		}		
 	}
 	
-	public ClientDAOImpl() throws SQLException {
-		preparedStatementGetById = DbConnection.getConnection().prepareStatement(GET_CLIENT_BY_ID);
-		preparedStatementSaveClient = DbConnection.getConnection().prepareStatement(SAVE_CLIENT);
-	}
-
 }
